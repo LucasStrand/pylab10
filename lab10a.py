317c5b2b4914a10bcdce1d83dfbf642e56f3c7cd
LucasStrand <57836852+LucasStrand@users.noreply.github.com>
2020-12-07 21:12:09 +0100

    Update lab10a.py

--- lab10a.py
+++ lab10a.py
@@ -1,5 +1,6 @@
 import re
 mtxt = "jox r.nohre@jth.hj.se, bjox@se, adam@example.com, jox@jox@jox.com."
 email = re.findall(r"\s\w+[._-]*\w+\@\w+\.*\w+\.\w+", mtxt)
+
 #email = re.findall(r"(\s\w+(?:\.\w+)*@\w+\.\w+(?:\\w+)*)", mtxt)
 print(email)
