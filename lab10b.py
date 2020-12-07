1a7a79127badf2cbe765b34dd004cd4a9485f6b3
LucasStrand <57836852+LucasStrand@users.noreply.github.com>
2020-12-07 11:37:11 +0100

    Update lab10b.py

--- lab10b.py
+++ lab10b.py
@@ -8,5 +8,13 @@ f = open("tabla.html", encoding="utf-8")
 txt = f.read()
 txt
 
-simpsons = re.findall(r'<td class="svtTablaTime">\s*(\d+\.\d+)\s*</td>\s*<td.*?>\s*<h4.*?>\s*Simpsons\s*</h4>\s*<div.*?>\s*<div.*?>\s*<div.*?>\s*<p.*?>\s*([a-zA-z0-9åäöÅÄÖ .,:;!?]+)*', txt )
-print (simpsons)
+simpsons = re.findall(r"""<td class="svtTablaTime">\s*(\d+\.\d+)\s*</td>\s*<td.*?>\s*<h4.*?>\s*Simpsons\s*</h4>\s*<div class="svtJsStopPropagation">\s*<div class="svtTablaTitleInfo svtHide-Js">\s*<div class="svtTablaContent-Description">\s*<p class="svtXMargin-Bottom-10px">\s*Amerikansk animerad komediserie från\s*\d+(?:\-*\d+)*. Säsong\s*(\d+)\.\s*Del\s*(\d+)\s*\w+\s*(\d+)\.\s*(.+?) Simpsons bor i Springfield.""", txt)
+    #print(result)
+    #print(len(result))
+for i in range(len(simpsons)):
+
+    print("Tid: ", simpsons[i][0])
+    print("Säsong: ", simpsons[i][1])
+    print("Avsnitt: ", simpsons[i][2],"/",simpsons[i][3])
+    print("Handling: ", simpsons[i][4])
+    print("---------------")
